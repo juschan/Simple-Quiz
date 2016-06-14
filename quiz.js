33d29e9d0513df81e0b62e44e9696aec3452eeaf
Justin Chan <jcsl76@yahoo.com>
2016-06-14 02:04:18 +0100

    Pseudocode for quiz with 2 players and 4 questions

---
+++ quiz.js
@@ -0,0 +1,57 @@
+//create some variables to store player 1 and 2 scores.
+//create a variable to store answers
+
+//start the quiz here
+//print first question to player 1
+
+//print the 4 options for the question
+
+//capture the answer from player 1
+
+//check if the answer from player 1 is correct
+//if answer is correct, add one to player 1 score.
+//if answer is wrong, do nothing.
+
+//switch to player 2.
+
+//print second question to player 1
+
+//print the 4 options for the question
+
+//capture the answer from player 2
+
+//check if the answer from player 2 is correct
+//if answer is correct, add one to player 2 score.
+//if answer is wrong, do nothing.
+
+
+//print third question to player 1
+
+//print the 4 options for the question
+
+//capture the answer from player 1
+
+//check if the answer from player 1 is correct
+//if answer is correct, add one to player 1 score.
+//if answer is wrong, do nothing.
+
+//switch to player 2.
+
+//print fourth question to player 1
+
+//print the 4 options for the question
+
+//capture the answer from player 2
+
+//check if the answer from player 2 is correct
+//if answer is correct, add one to player 2 score.
+//if answer is wrong, do nothing.
+
+
+//end of all questions
+//check to see who wins
+//if player 1 and 2 scores are the same, it is a draw.
+//if player 1 score is greater than player 2, player 1 wins.
+//if player 2 score is greater than player 1, player 2 wins.
+
+//end of quiz
